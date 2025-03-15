931d3c8cc02f12ed32d4928fb683d8adafa3225a
Leon Lonsdale <leon.codes@icloud.com>
2025-03-15 11:22:52 +0000

    add draw line method to window class

--- window.py
+++ window.py
@@ -1,5 +1,7 @@
 from tkinter import Tk, Canvas
 
+from line import Line
+
 
 class Window:
     def __init__(self, width: int, height: int) -> None:
@@ -10,13 +12,18 @@ class Window:
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line: Line, fill_color: str) -> None:
+        line.draw(self.__canvas, fill_color)
+
     def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self) -> None:
         self.__running = True
-        self.redraw()
+        while self.__running:
+            self.redraw()
+        print("Closed")
 
     def close(self) -> None:
         self.__running = False
