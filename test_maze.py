9ac3abfc76bfe2691aee7eafce23f7f7c0d2693a
Leon Lonsdale <leon.codes@icloud.com>
2025-03-16 16:04:04 +0000

    update test: test entrance and exit

--- test_maze.py
+++ test_maze.py
@@ -10,6 +10,8 @@ class Tests(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(len(m1._cells), num_cols)
         self.assertEqual(len(m1._cells[0]), num_rows)
+        self.assertEqual(m1._cells[0][0].has_top_wall, False)
+        self.assertEqual(m1._cells[-1][-1].has_bottom_wall, False)
 
 
 if __name__ == "__main__":
