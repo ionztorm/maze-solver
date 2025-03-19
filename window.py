544392dd185e43582d9b9a11413704be5f2e0d78
Leon Lonsdale <leon.codes@icloud.com>
2025-03-19 09:05:05 +0000

    update Window: spelling correction

--- window.py
+++ window.py
@@ -6,7 +6,7 @@ from line import Line
 class Window:
     def __init__(self, width: int, height: int) -> None:
         self.__root = Tk()
-        self.__root.title("Maze Solever")
+        self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, width=width, height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
